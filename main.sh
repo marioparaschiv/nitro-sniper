3fc750a91a2ca8c8176fef2a00ac65c45907a7e3
eternal <73202350+slow@users.noreply.github.com>
2021-10-26 18:38:42 +0100

    [1.4.5] Install dependencies if node_modules doesn't exist

--- main.sh
+++ main.sh
@@ -2,7 +2,7 @@ PACKAGE_VERSION=$(node -p -e "try { require('./nitro-sniper/package.json').versi
 LATEST_VERSION=$(curl --silent "https://raw.githubusercontent.com/slow/nitro-sniper/main/package.json" | grep '"version":' | sed -E 's/.*"([^"]+)".*/\1/')
 
 function compareVersions {
-   echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }'; 
+   echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }';
 }
 
 function installAll() {
@@ -10,6 +10,7 @@ function installAll() {
   install-pkg "http://archive.ubuntu.com/ubuntu/pool/main/g/glib2.0/libglib2.0-0_2.56.4-0ubuntu0.18.04.8_amd64.deb" &> /dev/null
   echo "$(tput setaf 2)Installed dependencies."
   echo "$(tput setaf 6)Cloning the latest sniper code..."
+  cd ..
   rm -rf nitro-sniper &> /dev/null
   git clone https://github.com/slow/nitro-sniper nitro-sniper &> /dev/null
   cd nitro-sniper
@@ -21,9 +22,9 @@ function installAll() {
   npx node ./src/index.js
 }
 
-if [ $(compareVersions $PACKAGE_VERSION) -lt $(compareVersions $LATEST_VERSION) ]; then 
+cd nitro-sniper
+if [ $(compareVersions $PACKAGE_VERSION) -lt $(compareVersions $LATEST_VERSION) ] || [ ! -d "node_modules" ]; then
   installAll
 else
-  cd nitro-sniper
   npx node ./src/index.js
 fi
