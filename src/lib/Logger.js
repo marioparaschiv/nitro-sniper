0942dcdd47ba794a81857e964d706565dad940b9
Craftzman7 <zezetta@icloud.com>
2022-03-10 05:57:39 +0100

    Add file logging

--- src/lib/Logger.js
+++ src/lib/Logger.js
@@ -1,5 +1,6 @@
 const chalk = require('chalk');
 const { Timestamp } = require('@skyra/timestamp');
+const fs = require("fs")
 
 module.exports = class Logger {
    constructor(options) {
@@ -8,22 +9,27 @@ module.exports = class Logger {
 
    debug(text, ...args) {
       if (!this.options.debug) return;
+      fs.appendFileSync("sniper.log", `${new Timestamp('YYYY-MM-DD HH:mm:ss')} DEBUG: ${text} ${args}`);
       return console.log(chalk`{bgGray.white [${new Timestamp('YYYY-MM-DD HH:mm:ss')}]} {gray ${text}}`, ...args);
    }
 
    warn(text, ...args) {
+      fs.appendFileSync("sniper.log", `${new Timestamp('YYYY-MM-DD HH:mm:ss')} WARNING: ${text} ${args}`);
       return console.log(chalk`{bgYellow.gray [${new Timestamp('YYYY-MM-DD HH:mm:ss')}]} {yellow ${text}}`, ...args);
    }
 
    success(text, ...args) {
+      fs.appendFileSync("sniper.log", `${new Timestamp('YYYY-MM-DD HH:mm:ss')} INFO: ${text} ${args}`);
       return console.log(chalk`{bgGreenBright.gray [${new Timestamp('YYYY-MM-DD HH:mm:ss')}]} {greenBright ${text}}`, ...args);
    }
 
    error(text, ...args) {
+      fs.appendFileSync("sniper.log", `${new Timestamp('YYYY-MM-DD HH:mm:ss')} ERROR: ${text} ${args}`);
       return console.log(chalk`{bgRed.white [${new Timestamp('YYYY-MM-DD HH:mm:ss')}]} {red ${text}}`, ...args);
    }
 
    critical(text, ...args) {
+      fs.appendFileSync("sniper.log", `${new Timestamp('YYYY-MM-DD HH:mm:ss')} CRITICAL ERROR: ${text} ${args}`);
       console.log(chalk`{bgRed.white [${new Timestamp('YYYY-MM-DD HH:mm:ss')}]} {red ${text}}`, ...args);
       return new Promise((fulfill) => {
          setTimeout(() => {
